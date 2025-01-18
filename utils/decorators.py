2f237e50e01e85a9ed58b098b6763ca954f71b9f
Andy Gnias <AGnias47@gmail.com>
2025-01-18 07:18:07 -0500

    Update docstring

--- utils/decorators.py
+++ utils/decorators.py
@@ -4,14 +4,20 @@ from time import time
 
 def timing(f):
     """
-    Taken from https://stackoverflow.com/a/27737385/8728749
+    Times a function. Returns the function result as a tuple of (result, time).
+
+    References
+    ----------
+    Taken from https://stackoverflow.com/a/27737385/8728749 and pulled again from
+    https://github.com/AGnias47/brats-challenge-cis-5528/blob/main/nn/nnet.py
+
     Parameters
     ----------
     f: function
 
     Returns
     -------
-
+    function result, time
     """
 
     @wraps(f)
