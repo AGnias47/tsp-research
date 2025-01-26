ccbf0fe48502a71fe71213a5ea9f01efc92444d4
Andy Gnias <AGnias47@gmail.com>
2025-01-26 16:52:19 -0500

    Add results

--- run.py
+++ run.py
@@ -2,16 +2,17 @@ import tsplib95
 
 from src.algorithms.concorde.concorde_wrapper import Concorde
 from src.algorithms.exact.brute_force import BruteForce
+from src.algorithms.exact.brute_force_optimized import BruteForceOptimized
 from src.algorithms.exact.dp import DP
 
 if __name__ == "__main__":
-    problems = ["papers/barachet10.tsp"]
+    problems = ["custom/p12.tsp"]
     for problem in problems:
         filepath = f"local/data/{problem}"
         name = tsplib95.load(filepath).name
         print(f"Solutions for the {name} problem")
         print("-----------------------")
-        for algorithm in [Concorde, DP, BruteForce]:
+        for algorithm in [BruteForce]:
             solver = algorithm(filepath)
             (best_cost, best_route), total_time = solver.run_tsp()
             print(f"Results of the {solver}")
