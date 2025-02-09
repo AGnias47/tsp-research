268a300df4f4375d2a7fbbd1e948acb244756fad
Andy Gnias <AGnias47@gmail.com>
2025-02-08 22:00:34 -0500

    Use optimized brute force

--- src/algorithms/brute_force.py
+++ src/algorithms/brute_force.py
@@ -17,21 +17,24 @@ class BruteForce(NetworkxTSP):
     def algorithm(self):
         best_cost = np.inf
         best_route = None
-        for permutation in permutations(self.G.nodes):
-            p_cost = 0
+        nodes = list(self.G.nodes)[1:]
+        for permutation in permutations(nodes):
+            permutation = [0] + list(permutation)
+            p_cost = float(0)
             p_route = np.empty(self.n + 1, dtype=int)
             p_route[0] = permutation[0]
             for i in range(self.n):
                 starting_node = permutation[i]
                 ending_node = permutation[(i + 1) % self.n]
                 p_cost += self.dist(starting_node, ending_node)
-                p_route[i] = ending_node
+                p_route[i + 1] = ending_node
             p_route[self.n] = p_route[0]
             if p_cost < best_cost:
                 best_cost = p_cost
                 best_route = p_route
         return best_cost, best_route
 
+
     @property
     def big_o_runtime(self):
-        return factorial(self.n)
+        return factorial(self.n-1)
