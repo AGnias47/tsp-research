bf3cc63fdb28029f1c51c4b82d27fbd5459b0c90
Andy Gnias <AGnias47@gmail.com>
2025-02-17 16:31:40 -0500

    Experimental script

---
+++ experiments.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python main.py -p barachet10 -a all -l
+python main.py -p gr17 -a concorde,as,mmas,q,dq,hk -l
+python main.py -p fri26,berlin52,brazil58,kroA100,a280 -a concorde,as,mmas,q,dq -l
