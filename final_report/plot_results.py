ec81a3a1f32b63ddcbfa98e3fd2a81e471421a49
Andy Gnias <AGnias47@gmail.com>
2025-04-07 20:14:06 -0400

    More mlflow

--- final_report/plot_results.py
+++ final_report/plot_results.py
@@ -31,20 +31,20 @@ def plot_symmetric():
     plt.show()
 
 
-def plot_symmetric():
-    problems_symmetric = ["fri26", "berlin52", "a280"]
-    concorde = [937, 7542, 2579]
-    nns = [1112, 8980, 3157]
-    mmas = [955, 8092, 2988]
-    dq = [1050, 9179, 5526]
-    dqn = [1184, 13462, 31082]
+def plot_asymmetric():
+    problems_asymmetric = ["br17", "ftv47", "rbg403"]
+    optimal = [39, 1776, 2465]
+    nns = [92, 2374, 3535]
+    mmas = [82, 2173, 5685]
+    dq = []
+    dqn = []
     fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(15, 5), sharex=False, sharey=False)
-    for i in range(len(problems_symmetric)):
-        ax[i].plot(problems_symmetric[i], concorde[i], "o", color="magenta", label="concorde")
-        ax[i].plot(problems_symmetric[i], nns[i], "o", color=[1.0, 0.45, 0.15], label="nns")
-        ax[i].plot(problems_symmetric[i], mmas[i], "o", color=[1.0, 0, 0], label="mmas")
-        ax[i].plot(problems_symmetric[i], dq[i], "o", color="purple", label="dq")
-        ax[i].plot(problems_symmetric[i], dqn[i], "o", label="dqn")
+    for i in range(len(problems_asymmetric)):
+        ax[i].plot(problems_asymmetric[i], optimal[i], "o", color="magenta", label="optimal")
+        ax[i].plot(problems_asymmetric[i], nns[i], "o", color=[1.0, 0.45, 0.15], label="nns")
+        ax[i].plot(problems_asymmetric[i], mmas[i], "o", color=[1.0, 0, 0], label="mmas")
+        ax[i].plot(problems_asymmetric[i], dq[i], "o", color="purple", label="dq")
+        ax[i].plot(problems_asymmetric[i], dqn[i], "o", label="dqn")
         ax[i].grid(True)
     fig.legend(*ax[0].get_legend_handles_labels(), loc="lower right")
     fig.supylabel("Cost")
