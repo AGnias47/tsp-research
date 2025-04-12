9a64554edd77e3e30b93fb617020a46d56198ee6
Andy Gnias <AGnias47@gmail.com>
2025-04-12 15:17:18 -0400

    Results generated

--- final_report/plot_results.py
+++ final_report/plot_results.py
@@ -5,6 +5,7 @@ Generating plots for the final report. Some disclaimers about this specific code
 
 https://github.com/AGnias47/russo-ukranian-tweet-classification/blob/main/sentiment_analysis/results.py
 https://stackoverflow.com/questions/33958068/matplotlib-how-to-plot-a-line-with-categorical-data-on-the-x-axis
+https://stackoverflow.com/a/10035974/8728749
 
 """
 
@@ -22,7 +23,7 @@ def plot_symmetric():
     fig, ax = plt.subplots(
         nrows=2, ncols=3, figsize=(13, 6), sharex=False, sharey=False
     )
-    # ax[0, 0].imshow(mpimg.imread("final_report/problem_images/berlin52.png"))
+    ax[0, 0].axis("off")
     ax[0, 0].set_xticks([])
     ax[0, 0].set_yticks([])
     ax[0, 1].imshow(mpimg.imread("final_report/problem_images/berlin52.png"))
@@ -45,7 +46,7 @@ def plot_symmetric():
         ax[1, i].plot(problems_symmetric[i], dqn[i], "o", label="dqn")
         ax[1, i].grid(True)
     fig.legend(*ax[1, 0].get_legend_handles_labels(), loc="lower right")
-    fig.supylabel("Cost                          Distance")
+    ax[1,0].set_ylabel("Cost")
     fig.suptitle("Symmetric Problem Images with Graphical Cost Results")
     plt.savefig("final_report/png/symmetric.png")
     plt.show()
@@ -79,42 +80,26 @@ def plot_star():
         ax[1, i].plot(problems_symmetric[i], dqn[i], "o", label="dqn")
         ax[1, i].grid(True)
     fig.legend(*ax[1, 0].get_legend_handles_labels(), loc="lower right")
-    fig.supylabel("Cost                          Distance")
+    ax[1,0].set_ylabel("Cost")
     fig.suptitle("Star Problem Images with Graphical Cost Results")
     plt.savefig("final_report/png/star.png")
     plt.show()
 
 
 def plot_spiral():
-    problems_symmetric = ["spiral79", "dh39"]
-    concorde = [286, 123]
-    mmas = [292, 140]
-    nns = [298, 129]
-    dq = [301, 128]
-    dqn = [306, 155]
-    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(8, 5), sharex=False, sharey=False)
-    ax[0, 0].imshow(mpimg.imread("final_report/problem_images/spiral79.png"))
-    ax[0, 0].set_xticks([])
-    ax[0, 0].set_yticks([])
-    # ax[0, 1].imshow(mpimg.imread("final_report/problem_images/dh39.png"))
-    # ax[0, 1].set_xticks([])
-    # ax[0, 1].set_yticks([])
-    for i in range(len(problems_symmetric)):
-        ax[1, i].plot(
-            problems_symmetric[i], concorde[i], "o", color="magenta", label="concorde"
-        )
-        ax[1, i].plot(
-            problems_symmetric[i], nns[i], "o", color=[1.0, 0.45, 0.15], label="nns"
-        )
-        ax[1, i].plot(
-            problems_symmetric[i], mmas[i], "o", color=[1.0, 0, 0], label="mmas"
-        )
-        ax[1, i].plot(problems_symmetric[i], dq[i], "o", color="purple", label="dq")
-        ax[1, i].plot(problems_symmetric[i], dqn[i], "o", label="dqn")
-        ax[1, i].grid(True)
-    fig.legend(*ax[1, 0].get_legend_handles_labels(), loc="lower right")
-    fig.supylabel("Cost                          Distance")
-    fig.suptitle("Spiral Problem Images with Graphical Cost Results")
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 3), sharex=False, sharey=False)
+    ax[0].imshow(mpimg.imread("final_report/problem_images/spiral79.png"))
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+    ax[1].plot("spiral79", 286, "o", color="magenta", label="concorde")
+    ax[1].plot("spiral79", 292, "o", color=[1.0, 0.45, 0.15], label="nns")
+    ax[1].plot("spiral79", 298, "o", color=[1.0, 0, 0], label="mmas")
+    ax[1].plot("spiral79", 301, "o", color="purple", label="dq")
+    ax[1].plot("spiral79", 306, "o", label="dqn")
+    ax[1].grid(True)
+    ax[1].set_ylabel("Cost")
+    fig.legend(*ax[1].get_legend_handles_labels(), loc="lower right")
+    fig.suptitle("Spiral Problem Image with Graphical Cost Results")
     plt.savefig("final_report/png/spiral.png")
     plt.show()
 
@@ -122,7 +107,6 @@ def plot_spiral():
 def plot_cluster():
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 3), sharex=False, sharey=False)
     ax[0].imshow(mpimg.imread("final_report/problem_images/5cluster43.png"))
-    ax[0].set_ylabel("Distance")
     ax[0].set_xticks([])
     ax[0].set_yticks([])
     ax[1].plot("5cluster43", 100, "o", color="magenta", label="concorde")
@@ -150,8 +134,12 @@ def plot_tetrahedral():
     ax[0, 0].imshow(mpimg.imread("final_report/problem_images/Tnm52.png"))
     ax[0, 0].set_xticks([])
     ax[0, 0].set_yticks([])
-    #ax[0, 1].imshow(mpimg.imread("final_report/problem_images/berlin52.png"))
-    #ax[0, 2].imshow(mpimg.imread("final_report/problem_images/a280.png"))
+    ax[0, 1].imshow(mpimg.imread("final_report/problem_images/Tnm70.png"))
+    ax[0, 1].set_xticks([])
+    ax[0, 1].set_yticks([])
+    ax[0, 2].imshow(mpimg.imread("final_report/problem_images/Tnm160.png"))
+    ax[0, 2].set_xticks([])
+    ax[0, 2].set_yticks([])
     for i in range(len(problems_symmetric)):
         try:
             ax[1, i].plot(
@@ -168,7 +156,7 @@ def plot_tetrahedral():
         ax[1, i].plot(problems_symmetric[i], dq[i], "o", color="purple", label="dq")
         ax[1, i].grid(True)
     fig.legend(*ax[1, 0].get_legend_handles_labels(), loc="lower right")
-    fig.supylabel("Cost                          Distance")
+    ax[1,0].set_ylabel("Cost")
     fig.suptitle("Tetrahedral Problem Images with Graphical Cost Results")
     plt.savefig("final_report/png/tetrahedral.png")
     plt.show()
@@ -196,7 +184,7 @@ def plot_asymmetrical_small_medium():
         ax[i].plot(problems_symmetric[i], dqn[i], "o", label="dqn")
         ax[i].grid(True)
     fig.legend(*ax[0].get_legend_handles_labels(), loc="lower right")
-    fig.supylabel("Cost                          Distance")
+    ax[0].set_ylabel("Cost")
     fig.suptitle("Asymmetrical Small-Medium Problems Graphical Cost Results")
     plt.savefig("final_report/png/asymmetric_small-medium.png")
     plt.show()
@@ -204,15 +192,15 @@ def plot_asymmetrical_small_medium():
 
 def plot_asymmetrical_large():
     problems_symmetric = ["rbg323", "rbg358", "rbg403"]
-    optimum = [39, 1776]
-    mmas = [82, 2173]
-    nns = [92, 2374]
-    dq = [92, 2374]
-    dqn = [92, 3046]
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 5), sharex=False, sharey=False)
+    optimum = [1326, 1163, 2465]
+    nns = [1734, 1812, 3535]
+    mmas = [3488, 4180, 5685]
+    dq = [2245, 1817, 3457]
+    dqn = [6119, 6711, 7575]
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(8, 5), sharex=False, sharey=False)
     for i in range(len(problems_symmetric)):
         ax[i].plot(
-            problems_symmetric[i], optimum[i], "o", color="magenta", label="hk/optim"
+            problems_symmetric[i], optimum[i], "o", color="magenta", label="optim"
         )
         ax[i].plot(
             problems_symmetric[i], nns[i], "o", color=[1.0, 0.45, 0.15], label="nns"
@@ -224,15 +212,16 @@ def plot_asymmetrical_large():
         ax[i].plot(problems_symmetric[i], dqn[i], "o", label="dqn")
         ax[i].grid(True)
     fig.legend(*ax[0].get_legend_handles_labels(), loc="lower right")
-    fig.supylabel("Cost                          Distance")
-    fig.suptitle("Asymmetrical Small-Medium Problems Graphical Cost Results")
+    ax[0].set_ylabel("Cost")
+    fig.suptitle("Asymmetrical Large Problems Graphical Cost Results")
     plt.savefig("final_report/png/asymmetric_small-medium.png")
     plt.show()
 
 if __name__ == "__main__":
     plot_symmetric()
-    # plot_star()
-    # plot_spiral()
-    # plot_cluster()
-    # plot_tetrahedral()
-    # plot_asymmetrical_small_medium()
+    plot_star()
+    plot_spiral()
+    plot_cluster()
+    plot_tetrahedral()
+    plot_asymmetrical_small_medium()
+    plot_asymmetrical_large()
