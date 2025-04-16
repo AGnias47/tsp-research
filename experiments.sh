8843d2b7a2530156d8d64b847d133d23995406ab
Andy Gnias <AGnias47@gmail.com>
2025-04-15 21:13:55 -0400

    Remove experiments file

--- experiments.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-python main.py -p barachet10 -a all -l
-python main.py -p gr17 -a concorde,as,mmas,q,dq,hk,nns -l
-python main.py -p fri26,berlin52,brazil58,kroA100,a280 -a concorde,as,mmas,q,dq,nns -l
-python main.py -p a280 -a q,dq -l
